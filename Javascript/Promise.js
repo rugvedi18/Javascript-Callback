cc36dacf77fb4e847d35b9580c03bc4bf2e02857
Rugvedi <66817023+rugvedi18@users.noreply.github.com>
2022-06-08 11:41:59 +0530

    Update with use case statement

--- Javascript/Promise.js
+++ Javascript/Promise.js
@@ -1,41 +1,42 @@
+// use case - used to handle async operations when callbacks create callback hell (nested callbacks or functions) leads to unmanageable code
 
-// Exzample 1
+// Example 1
 
-// let process = new Promise((resolve, reject) => {
-//     let a = 4;
-//     if (a == 3) {
-//         resolve('Success');
-//     }
-//     else {
-//         reject('Failed');
-//     }
-// })
-// process
-//     .then((message) => {
-//         console.log(message)
-//     })
-//     .catch((message) => {
-//         console.log(message)
-//     })
+let process = new Promise((resolve, reject) => {
+    let a = 4;
+    if (a == 3) {
+        resolve('Success');
+    }
+    else {
+        reject('Failed');
+    }
+})
+process
+    .then((message) => {
+        console.log(message)
+    })
+    .catch((message) => {
+        console.log(message)
+    })
 
 
 // Example 2
 
-// function process(a, b) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             // resolve(a + b);
-//             reject(a + b);
-//         }, 1000)
-//     });
-// }
-// process(1, 2)
-//     .then((value) => {
-//         console.log("Value is " + value)
-//     })
-//     .catch((err) => {
-//         console.log("Error " + err)
-//     })
+function process(a, b) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // resolve(a + b);
+            reject(a + b);
+        }, 1000)
+    });
+}
+process(1, 2)
+    .then((value) => {
+        console.log("Value is " + value)
+    })
+    .catch((err) => {
+        console.log("Error " + err)
+    })
 
 // Example 3
 
